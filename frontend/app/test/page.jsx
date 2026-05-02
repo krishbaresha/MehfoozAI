4349ba8f7bd6d97e995ebfc27d720d40ce28160e
krishbaresha <krishbaresha@gmail.com>
2026-05-02 18:21:46 +0500

    Debug: Add test page and remove index.html

---
+++ frontend/app/test/page.jsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: 100, textAlign: 'center' }}>
+      <h1>Test Page</h1>
+      <p>If you see this, Next.js routing is working!</p>
+    </div>
+  );
+}
